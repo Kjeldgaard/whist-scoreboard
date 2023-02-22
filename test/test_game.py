b2b1ab4db59b88f43221933f974a1f59796c516a
Kjeldgaard <Kjeldgaard@users.noreply.github.com>
2023-02-23 00:06:43 +0100

    Fix lint and extend code covearge to all branches

--- test/test_game.py
+++ test/test_game.py
@@ -3,7 +3,7 @@ from game import Game
 
 def test_game_begin(monkeypatch):
     inputs = iter(["-100", "Niels", "Anders", "Mads", "Stef", "q"])
-    monkeypatch.setattr('builtins.input', lambda name: next(inputs))
+    monkeypatch.setattr("builtins.input", lambda name: next(inputs))
     game = Game()
     game.begin_game()
     score = game.get_total_score()
@@ -11,12 +11,26 @@ def test_game_begin(monkeypatch):
 
 
 def test_game_add_round(monkeypatch):
-    inputs = iter(["-100", "Niels", "Anders", "Mads", "Stef", "q", "10", "r", "10", "w", "l", "w", "l"])
-    monkeypatch.setattr('builtins.input', lambda name: next(inputs))
+    inputs = iter(
+        [
+            "-100",
+            "Niels",
+            "Anders",
+            "Mads",
+            "Stef",
+            "q",
+            "10",
+            "r",
+            "10",
+            "w",
+            "l",
+            "w",
+            "l",
+        ]
+    )
+    monkeypatch.setattr("builtins.input", lambda name: next(inputs))
     game = Game()
     game.begin_game()
     game.add_round()
     score = game.get_total_score()
     assert score == -400
-
-
